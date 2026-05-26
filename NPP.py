c97c276278c6e3f6cac464872237a02da6742b0a
tincho5294-star <tincho5294@gmail.com>
2026-05-27 08:48:22 +0900

    SMOL change.

--- NPP.py
+++ NPP.py
@@ -427,7 +427,7 @@ class Knob:
 
         elif self._type==2:
             for i in range(self.amin_2-60,(self.amax_2+60)-1,15):
-                value_t=(i-(self.amin_2))/((self.amin_2+360)-(self.amax_2))
+                value_t=(i-(self.amin_2-60))/((self.amin_2+360)-(self.amax_2))
                 i_value=int(lerp(0,100,value_t))
                 value_text=dial_font.render(str(i_value),True,(175,175,175))
                 ix=self.x-math.cos(math.radians(normalize360(i)))*25
