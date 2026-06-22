c8d2d358e0534053b3bc9eebb07e53850ab214e3
tincho <tincho5294@gmail.com>
2026-06-23 08:59:48 +0900

    quick commit

--- PuRGATORY.py
+++ PuRGATORY.py
@@ -522,7 +522,8 @@ class PlayerManager:
         self.fired=False
     def update(self,style,max_style,min_style):
         self.hard=(self.max_health-self.min_health)-self.health
-        self.health=lerp(self.health,self.max_health*(style-min_style)/(max_style-min_style),dt if self.health<=self.hard else dt*0.001)
+        target_health=self.max_health*(style-min_style)/(max_style-min_style)
+        self.health=lerp(self.health,target_health,dt/(1+(self.hard/100)) if target_health>=self.hard else dt)
 class Meter:
     def __init__(self,x,y,w,h,value,min_value,max_value,timeline_length):
         self.x=x
