f00860c83818b30ce221cb81344ec13f8c46bc85
tincho5294-star <tincho5294@gmail.com>
2026-06-23 21:01:06 +0900

    no balls you wont

--- PuRGATORY.py
+++ PuRGATORY.py
@@ -523,9 +523,9 @@ class PlayerManager:
         self.fired=False
     def update(self,style,max_style,min_style):
         self.hard=(self.max_health-self.min_health)-self.health
-        target_health=self.max_health*((style-min_style)/(max_style-min_style))
+        target_health=(self.max_health-self.min_health)*((style-min_style)/(max_style-min_style))
         self.next_health=lerp(self.health,target_health,dt)
-        self.health=lerp(self.health,self.next_health,0.001/(1+(self.hard/100)) if self.next_health>((self.max_health-self.min_health)-self.hard) else 1)
+        self.health=lerp(self.health,self.next_health,0.001/(1+(self.hard/100)) if (self.next_health>((self.max_health-self.min_health)-self.hard)) else 1)
 class Meter:
     def __init__(self,x,y,w,h,value,min_value,max_value,timeline_length):
         self.x=x
