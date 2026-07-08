f8008cbf4d2d77810eefcb15b8c2cd73a94d651d
tincho <tincho5294@gmail.com>
2026-07-08 10:56:10 +0900

    nga commit

--- PuRGATORY.py
+++ PuRGATORY.py
@@ -1077,7 +1077,7 @@ class Reactor:
                         current=self.water_entry[i]
                         later=self.water_entry[(i+1)] if i<625 else None
                         if previous["progress"]-current["progress"]<=dt:
-                            previous["amount"],current["amount"]=heat_exchange(previous["amount"],current["amount"],abs(current["velocity"]-previous["velocity"]),dt)
+                            previous["amount"],current["amount"]=heat_exchange(previous["amount"],current["amount"],clamp(previous["velocity"]-current["velocity"],0,1),dt)
                             
                     
 class Pump:
