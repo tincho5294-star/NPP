e83751fa637a9e69691e45e229d15c561889b92e
tincho <tincho5294@gmail.com>
2026-09-01 17:22:49 +0900

    폰으로도 파일 수정이 가능하구나

--- PuRGATORY.py
+++ PuRGATORY.py
@@ -47,7 +47,7 @@ def heat_exchange(a_temp,b_temp,a_mass,b_mass,flow_rate,dt):
     t=flow_rate*dt
     new_a_temp=a_temp+(b_temp-a_temp)/(a_mass+1e-6)*t
     new_b_temp=b_temp+(a_temp-b_temp)/(b_mass+1e-6)*t
-    if 1/(a_mass+1e-6)*t>0.5 and 1/(b_mass+1e-6)*t>0.5:
+    if 1/(a_mass+1e-6)*t>=1 and 1/(b_mass+1e-6)*t>=1:
         balance=(new_a_temp+new_b_temp)/2
         new_a_temp=new_b_temp=balance
     new_a_temp=clamp(new_a_temp,min(new_a_temp,new_b_temp),max(new_a_temp,new_b_temp))
