fa6dd17c5e162930f486d627d993ed5e46daf85f
tincho <tincho5294@gmail.com>
2026-09-22 12:39:13 +0900

    itadori yuji.

--- PuRGATORY.py
+++ PuRGATORY.py
@@ -1066,7 +1066,7 @@ class GridCell:
                 self.max_mass=clamp(self.max_mass,0,7000)
                 self.max_level=clamp(self.max_level,0,7000)
                 self.owner.temp,self.temp=heat_exchange(self.owner.temp,self.temp,3500*(self.owner.uranium_mass/3.5),self.mass,(0.1+((self.water_velocity*0.9)/100)*(self.level/7000))*self.turbulence_intensity,dt)
-                Pvoid=(self.void*461.5*self.void_temp)/(self.void*(self.void_temp**0.016))
+                Pvoid=(self.void*461.5*self.void_temp)/(self.void*(self.void*(self.void_temp**0.016)))
                 Pwater=self.density*g/self.level
                 self.pressure=((self.mass+self.void*1600*self.temp)-(0.5*self.density*self.water_velocity**2))/101325
                 for n in self.neighbors:
